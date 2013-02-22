f43385cc4063308b9203eb1e09b1ce051967b93d
Apprentice <apprentice@dbc24.local>
2013-02-22 15:55:59 -0800

    all the rest of the logic

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,11 +1,11 @@
 post '/games/:id/guesses' do
-  guess = Guess.new(params) #game_id and #card_id
-  guess.save!
+  guess = Guess.new(:game_id => params[:game_id], :card_id => params[:card_id]) #game_id and #card_id
+  guess.save
   card = Card.find(params[:card_id])
-  if card.answer == answer
-    card.update_attribute(:correct, true)
+  if card.answer == params[:guess]
+    guess.update_attribute(:correct, true)
   else
-    card.update_attribute(:correct, false)
+    guess.update_attribute(:correct, false)
   end
   game = Game.find(params[:game_id])
   game.update_attribute(:current_card, game.current_card + 1)
