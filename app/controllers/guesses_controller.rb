309999626460e7416b45c47871c51c39c51e8e0f
Brayden Cleary <brayden.cleary@gmail.com>
2013-02-24 11:52:47 -0800

    Refactored guesses controller

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,25 +1,16 @@
 post '/games/:id/guesses' do
-  guess = Guess.new(:game_id => params[:game_id], :card_id => params[:card_id]) #game_id and #card_id
-  guess.save
+  guess = Guess.create(:game_id => params[:game_id], :card_id => params[:card_id]) #game_id and #card_id
   card = Card.find(params[:card_id])
-  @game = Game.find(params[:game_id])
-  cards = @game.deck.cards
-  if card.answer.downcase == params[:guess].downcase
-    guess.update_attribute(:correct, true)
-    @message = "you got it right :)"
-  else
-    guess.update_attribute(:correct, false)
-    @message = "you got it wrong :("
-  end
-
-  @game.update_attribute(:current_card, @game.current_card + 1)
-  @current_card = cards[@game.current_card]
-  if @game.current_card == cards.length
-    @game.update_attribute(:completed_at, Time.now)
-    redirect to "/games/#{@game.id}/result"
+  if card.check_answer(params[:guess]) 
+    guess.is_correct
+    session[:question_result] = "Correct!"
   else 
-    erb :game
+    guess.is_incorrect
+    session[:question_result] = "Incorrect."
   end
+  game = Game.find(params[:game_id])
+  game.update_attribute(:current_card, game.current_card + 1)
+  redirect to "/games/#{game.id}"
 end
 
 
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,8 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
+
+  def check_answer(guess)
+    self.answer.downcase == guess.downcase ? true : false
+  end
+
 end
