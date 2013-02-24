2d99a33f57b94c851f1c615379042139d8ab24f9
Brayden Cleary <brayden.cleary@gmail.com>
2013-02-24 14:24:22 -0800

    Ajaxified flashcard creation

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,29 +1,21 @@
 $(document).ready(function(){ 
-
-  $('#response form').on('submit', function(e) {
-
+  $('.add_new_card').submit(function(e){
     e.preventDefault();
-    var $self = $(this);
-    var params = $self.serialize();
 
-    $.ajax({
-      type:     $self.attr('method'),
-      url:      $self.attr('action'),
-      dataType: 'json',
-      data:     params,
-      success: function(data, status, xhr) {
-        $('.stat_column').html('hello')
+    $.ajax({ //What JQuery object are we selecting here?  Are you calling on jQuery class?
+      type: this.method,
+      url: this.action,
+      data: $(this).serialize(),
+      dataType: "json",
+      success: function(data, status, xhr){
+        var message = 'New card created!';
+        $('h2').html(message);
+        var newCard = "<li>Question: " + data.question + "</li><li>Answer: "+ data.answer + "</li><br>";
+        $("ul.card-list li:first").hide().prepend(newCard).slideDown();
+        $('.add_new_card textarea').val('');
       }
-      error: function(xhr, status, error) {
-
-      }
-      complete: function(xhr, status) {
-
-      }
-
     });
-
   });
-
 });
+
 
